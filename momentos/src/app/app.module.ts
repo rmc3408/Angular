dc62e6df16843f758560eca5ada6d53365d7fb4e
Raphael <rmc3408@protonmail.com>
2023-06-04 20:00:55 -0400

    matheus: video 10

--- momentos/src/app/app.module.ts
+++ momentos/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent
+    UserComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule
